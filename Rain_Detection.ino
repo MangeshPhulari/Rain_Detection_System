763343ff9d7c50f8d235739ae7b2512cb667c70b
MangeshPhulari <113586022+MangeshPhulari@users.noreply.github.com>
2024-07-18 14:48:59 +0530

    Rain_Detection.ino

--- Rain_Detection.ino
+++ Rain_Detection.ino
@@ -1,41 +1,89 @@
+#include <SoftwareSerial.h>
 
-int rainAnalogPin = A0;
-int rainDigitalPin = 2;
-int redLED = 3;
-int alarm = 5; //PWM (~) Pin
+String _buffer;
+String number = "+918482878905";
 
-void setup(){
-  pinMode(rainAnalogPin, INPUT);
-  pinMode(rainDigitalPin, INPUT);
-  pinMode(alarm, OUTPUT);
-  pinMode(redLED, OUTPUT);
-  digitalWrite(alarm, LOW);
-  digitalWrite(redLED, LOW);
+#define LIQUID_SENSOR A1
+
+int liquid_value = 0;
+
+int liquid_default = 100;
+//liquid_flag = 0 means liquid not detected
+//liquid_flag = 1 means liquid is detected
+boolean liquid_flag = 0;
+
+SoftwareSerial mySerial(2, 3);
+
+#define BUZZER_PIN 9
+
+void setup()
+{
+  mySerial.begin(9600);
+  //Begin serial communication: SIM800L
   Serial.begin(9600);
+  //Begin serial communication: Arduino IDE (Serial Monitor)
+
+  pinMode(LIQUID_SENSOR, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  //by default the BUZZER is OFF
+  digitalWrite(BUZZER_PIN, LOW); 
+  Serial.println("Initializing...");
+  //Once the handshake test is successful, it will back to OK
+  mySerial.println("AT");
+  delay(1000);
+  mySerial.println("AT+CMGF=1");
+  delay(1000);
+
 }
 
-void loop() {
-  // read the input on analog pin 0:
-  int sensorAnalogValue = analogRead(rainAnalogPin);
-  Serial.print("Rain sensor analog value: ");
-  Serial.println(sensorAnalogValue);
-  
-  // read the input on digital pin 2:
-  int sensorDigitalValue = digitalRead(rainDigitalPin);
-  Serial.print("Rain sensor digital value: ");
-  Serial.println(sensorDigitalValue);
-  
-//digital output
- if(sensorAnalogValue <=500) //High means rain detected; In some case vise versa
- {
-  digitalWrite(redLED, HIGH);
-  tone(alarm, 5000); //the buzzer sound frequency at 5000 Hz
-  Serial.println("Rain Detected");
- }
- else {
-  digitalWrite(redLED, LOW);
-  noTone(alarm);
-  Serial.println("No Rain Detected");
- }
- delay(250);
+void loop()
+{
+  while (mySerial.available()) {
+    Serial.println(mySerial.readString());
+  }
+
+  liquid_value = analogRead(LIQUID_SENSOR);
+  liquid_value = map(liquid_value, 0, 1023, 225, 0);
+  Serial.println(liquid_value);
+  //The liquid is detected, trigger Alarm and send sms
+  if (liquid_value >= liquid_default) {
+    digitalWrite(BUZZER_PIN, HIGH);
+    liquid_flag = 1;
+    if (liquid_flag == 1) {
+      Serial.println("Liquid is Detected.");
+      send_sms();
+      make_call();
+      liquid_flag = 0;
+
+    }
+  }
+  //No liquid is detected, turn OFF Alarm
+  else {
+    digitalWrite(BUZZER_PIN, LOW);
+    if (liquid_flag == 0) {
+      Serial.println("Liquid is not Detected.");
+    }
+  }
+
+}
+void make_call() {
+  mySerial.print (F("ATD"));
+  mySerial.print (number);
+  mySerial.print (F(";\r\n"));
+  //_buffer = _readSerial();
+  Serial.println(_buffer);
+}
+
+void send_sms()
+{
+  Serial.println("sending sms....");
+  delay(50);
+  mySerial.print("AT+CMGF=1\r");
+  delay(1000);
+  mySerial.print("AT+CMGS=\"" + number + "\"\r");
+  delay(1000);
+  mySerial.print("Liquid has been detected");
+  delay(100);
+  mySerial.write(26); //ascii code for ctrl-26 //Serial2.println((char)26); //ascii code for ctrl-26
+  delay(5000);
 }
